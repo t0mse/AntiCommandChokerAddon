08aae98fa760788db8d3fb8badce8a8f12d696ea
t0mse <thetomseofficial@gmail.com>
2022-07-06 13:37:21 +0200

    Add check if not only a normal number beginning with '7' was attempted to send

--- src/main/java/de/t0mse/labymod/addon/anticommandchoker/listener/subscriber/CommandChokerSubscriber.java
+++ src/main/java/de/t0mse/labymod/addon/anticommandchoker/listener/subscriber/CommandChokerSubscriber.java
@@ -3,10 +3,15 @@ package de.t0mse.labymod.addon.anticommandchoker.listener.subscriber;
 import de.t0mse.labymod.addon.anticommandchoker.listener.IMessageSendSubscriber;
 import de.t0mse.labymod.addon.anticommandchoker.util.ChatMessageUtil;
 
+import java.util.regex.Pattern;
+
 public final class CommandChokerSubscriber implements IMessageSendSubscriber {
     @Override
     public boolean onSend(String command, String wholeMessage) {
-        if (command.length() > 1 && command.startsWith("7")) {
+        final String chokeTrigger = "7";
+        if (Pattern.matches("[a-zA-Z]+", command.replace(chokeTrigger, ""))
+                && command.length() > 1
+                && command.startsWith(chokeTrigger)) {
             SendAnywaySubscriber.redefineCommandTriggerSuffix();
             ChatMessageUtil.sendMessage("§cRecognized §cchoked §ccommand!");
             ChatMessageUtil.sendMessage("§7[SEND §7MESSAGE §7ANYWAY]",
